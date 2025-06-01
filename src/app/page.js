041f212a06bb81312fa612480734307bc32238f8
Aditya <aadimalviyaa@gmail.com>
2025-06-01 15:54:54 +0530

    feat/add-video-1st-june

--- src/app/page.js
+++ src/app/page.js
@@ -185,7 +185,7 @@ const Home = () => {
       <div className={`w-full relative   ${isScroll ? 'h-[240px] md:h-[812px]' : 'h-[240px]  md:h-screen'}`}  >
         <video
           className={`w-full object-fill  ${isScroll ? 'h-[240px] md:h-[812px]' : 'h-[240px]  md:h-screen'}`}
-          src="/video/banner.mov"
+          src="/video/1StJune.mp4"
           controls
           muted
           autoPlay
