8195212c98ea13b2e16e3855f4559fbd65336aea
Aaditya0806 <aadimalviyaa@gmail.com>
2025-12-06 02:54:52 +0530

    add-home-product-quality-unit

--- src/app/page.js
+++ src/app/page.js
@@ -291,7 +291,7 @@ const Home = () => {
                          <SwiperSlide>
                             <video
                             className={`w-full object-fill  h-[240px] md:h-[812px]`}
-                            src="/video/Diwali.mp4"
+                            src="/video/dec-25-home.mp4"
                             controls
                             muted
                             autoPlay
