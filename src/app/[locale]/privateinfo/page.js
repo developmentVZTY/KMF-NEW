b7d07e7cb0e851796a204c23dca91b78024ddc84
Aaditya0806 <aadimalviyaa@gmail.com>
2026-01-28 13:55:10 +0530

    add-private-info-26-updated

--- src/app/[locale]/privateinfo/page.js
+++ src/app/[locale]/privateinfo/page.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Header } from '@/components/Header'
-import secretinfoImg from "@/images/Privateinfo-new.png"
+// import secretinfoImg from "@/images/Privateinfo-new.png"
+import secretinfoImg from "@/images/Private-info-26.png"
+import Footer from '@/components/Footer'
 
 function PrivateInfo() {
   return (
 
    <div className='w-full h-full'>
       
-      <div className='w-full h-[98vh] flex justify-center items-center'>
+      <div className='w-full md:h-[98vh] flex justify-center '>
          
-         <div className='w-[95%] md:w-[48%] h-auto'>
+         <div className='w-[95%] md:w-[45%] mt-10 h-auto'>
             <img className='w-full h-full' src={secretinfoImg.src} alt="" />
          </div>
-            
+          
       </div>
+       <div className='hidden lg:block w-full h-[400px]'></div> 
+     
       
     </div>
   )
