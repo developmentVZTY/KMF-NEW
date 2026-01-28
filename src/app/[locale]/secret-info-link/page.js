aedadce9f5ea8cd37169ca81780807ba4c787e3e
Aaditya0806 <aadimalviyaa@gmail.com>
2026-01-28 13:37:32 +0530

    add-new-scret-info-link-26

--- src/app/[locale]/secret-info-link/page.js
+++ src/app/[locale]/secret-info-link/page.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Header } from '@/components/Header'
-import secretinfoImg from "@/images/scret-info-1.jpeg"
+// import secretinfoImg from "@/images/scret-info-1.jpeg"
+import secretinfoImg from "@/images/scret-info-26.png"
 
 function SecretInfoLink() {
   return (
 
     <div className='w-full h-full'>
       
-      <div className='w-full h-[98vh] flex justify-center items-center'>
+      <div className='w-full md:h-[98vh]  flex justify-center items-center'>
          
-         <div className='w-[95%] md:w-[55%] h-auto'>
+         <div className='w-[95%] md:w-[45%] h-auto'>
             <img className='w-full h-full' src={secretinfoImg.src} alt="" />
          </div>
             
